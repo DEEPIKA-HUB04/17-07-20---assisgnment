2192cc8b9f26b6f0f4f6c5b9679224dc5a0a540b
DEEPIKA-HUB04 <66515582+DEEPIKA-HUB04@users.noreply.github.com>
2020-07-20 18:45:55 +0530

    Add files via upload

---
+++ map/map.js
@@ -0,0 +1,3 @@
+let arr = [11,27,30,44,53,69,77,80,95,101];
+let a = arr.map((arr) =>  arr+10 )
+       console.log(a);
---
+++ map/map_output.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var arr = [11, 27, 30, 44, 53, 69, 77, 80, 95, 101];
+var a = arr.map(function (arr) {
+  return arr + 10;
+});
+console.log(a);
