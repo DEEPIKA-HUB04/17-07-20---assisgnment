72f471f5895e51619770064f8fcbfa2a7de87e84
DEEPIKA-HUB04 <66515582+DEEPIKA-HUB04@users.noreply.github.com>
2020-07-20 19:39:03 +0530

    Add files via upload

---
+++ sort/output.js
@@ -0,0 +1,36 @@
+"use strict";
+
+var items = [{
+  name: 'Edward',
+  value: 21
+}, {
+  name: 'Sharpe',
+  value: 37
+}, {
+  name: 'And',
+  value: 45
+}, {
+  name: 'The',
+  value: -12
+}, {
+  name: 'Magnetic',
+  value: 13
+}, {
+  name: 'Zeros',
+  value: 37
+}];
+var result = items.sort(function (a, b) {
+  var nameA = a.name.toUpperCase();
+  var nameB = b.name.toUpperCase();
+
+  if (nameA < nameB) {
+    return -1;
+  }
+
+  if (nameA > nameB) {
+    return 1;
+  }
+
+  return 0;
+});
+console.log(result);
